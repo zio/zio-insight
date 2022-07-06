94b04a04d4eed5b922c65c8ee3264363d693f907
Mohammad Yousuf Minhaj Zia <yzia2000@gmail.com>
2022-07-06 13:38:51 +0530

    feat: changed package from zio.zmx.client -> zio.insights
    Renamed packages and updated folder path.
    Removed dist

---
+++ zio-insights/js/src/main/scala/zio/insights/frontend/Main.scala
@@ -0,0 +1,19 @@
+package zio.insights.frontend
+
+import com.raquo.laminar.api.L._
+
+import org.scalajs.dom
+
+import zio.insights.frontend.views._
+
+object Main {
+
+  def main(args: Array[String]): Unit = {
+    val _ = documentEvents.onDomContentLoaded.foreach { _ =>
+      val appContainer = dom.document.querySelector("#app")
+      appContainer.innerHTML = ""
+      val _            = render(appContainer, MainView.render)
+    }(unsafeWindowOwner)
+  }
+
+}
---
+++ zio-insights/js/src/main/scala/zio/insights/frontend/views/VegaChart.scala
@@ -0,0 +1,59 @@
+package zio.insights.frontend.views
+
+import scala.scalajs.js
+import scala.util.Failure
+
+import com.raquo.laminar.api.L._
+
+import org.scalajs.macrotaskexecutor.MacrotaskExecutor.Implicits._
+
+import zio.insights.frontend.model._
+import zio.insights.frontend.model.PanelConfig.DisplayConfig
+import zio.insights.frontend.state.AppState
+import zio.insights.frontend.vega.Vega
+
+object VegaChart {
+
+  def render($cfg: Signal[DisplayConfig]): HtmlElement =
+    new VegaChartImpl().render($cfg)
+
+  final private class VegaChartImpl() {
+
+    private def update(el: HtmlElement, cfg: DisplayConfig): HtmlElement = {
+      Vega
+        .embed(
+          el.ref,
+          VegaModel(cfg, AppState.recordedData.now().getOrElse(cfg.id, LineChartModel(cfg.maxSamples))).vegaDef,
+          js.Dynamic.literal("logLevel" -> "Debug", "actions" -> false)
+        )
+        .toFuture
+        .onComplete {
+          case Failure(exception) =>
+            println(exception.getMessage)
+          case _                  => // do nothing
+        }
+
+      el
+    }
+
+    def render($cfg: Signal[DisplayConfig]): HtmlElement =
+      div(
+        cls := "w-full h-full",
+        children <-- $cfg.map { cfg =>
+          Seq(
+            div(
+              cls := "w-full h-full",
+              child <-- AppState.updatedData.events.filter(_ == cfg.id).map { _ =>
+                div(
+                  cls := "w-full h-full",
+                  inContext { el =>
+                    update(el, cfg)
+                  }
+                )
+              }
+            )
+          )
+        }
+      )
+  }
+}
