56d27c856f3d41003294ffbe2da82cb797998cb6
Mohammad Yousuf Minhaj Zia <yzia2000@gmail.com>
2022-07-06 12:51:29 +0530

    feat: created sbt project for zio-insights
    Using sbt 1.6.2
    ScalaJS, laminar, zio dependencies included
    Version constants in project/Version.scala
    Using zio 2.0.0
    Scala cross compile with 2.12.14, 2.13.8 and 3.1.2

---
+++ project/Version.scala
@@ -0,0 +1,18 @@
+object Version {
+  val Scala211   = "2.11.12"
+  val Scala212   = "2.12.15"
+  val Scala213   = "2.13.8"
+  val ScalaDotty = "3.1.2"
+
+  val zio     = "2.0.0"
+  val zioJson = "0.3.0-RC4"
+  val zioHttp = "2.0.0-RC9"
+
+  val animus              = "0.1.9"
+  val boopickle           = "1.4.0"
+  val fansi               = "0.2.14"
+  val laminar             = "0.14.2"
+  val laminext            = "0.14.3"
+  val scalaJsSecureRandom = "1.0.0"
+  val scalaJavaTime       = "2.3.0"
+}
